a2240733488894c97b5756fa712d8545d9e3587f
Eric Tendian <erictendian@gmail.com>
2014-12-30 19:58:46 -0600

    Adding pagination to BBCode command handler

--- app/YSFHQ/Migrator/Commands/FixBbcodeCommandHandler.php
+++ app/YSFHQ/Migrator/Commands/FixBbcodeCommandHandler.php
@@ -15,22 +15,23 @@ class FixBbcodeCommandHandler implements CommandHandler
 
     public function handle($command)
     {
-        $posts = $this->client->getPosts(1);
+        $page = 1;
+        while ($posts = $this->client->getPosts($page)) {
+            foreach ($posts as $post) {
+                if ($post['bbcode_bitfield']) {
 
-        foreach ($posts as $post) {
+                    $bitfield = new BitfieldHelper($post['bbcode_bitfield']);
 
-            if ($post['bbcode_bitfield']) {
-
-                $bitfield = new BitfieldHelper($post['bbcode_bitfield']);
-
-                if (in_array(88, $bitfield->get_all_set())) {
-                    $bitfield->clear(88);
-                    $bitfield->set(130);
-                    $new_bitfield = $bitfield->get_base64();
-                    // update
+                    if (in_array($command->old_bbcode_id, $bitfield->get_all_set())) {
+                        $bitfield->clear($command->old_bbcode_id);
+                        $bitfield->set($command->new_bbcode_id);
+                        $new_bitfield = $bitfield->get_base64();
+                        echo $new_bitfield;
+                        // update
+                    }
                 }
             }
-
+            $page++;
         }
     }
 
